5417b09fee2c54d6de33b7bdd9e8e4769c308626
Kirsten Werner <kirstenmwerner@gmail.com>
2019-02-27 16:12:20 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -6,6 +6,13 @@ class Patient
   def initialize(name)
     @name = name 
     @@all << self 
-  end
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def new_appointment(doctor, date)
+    Appointment.new()
   
 end 
