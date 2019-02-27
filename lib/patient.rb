861ecd08c90f630f12e641cecaef7769b19238ff
Kirsten Werner <kirstenmwerner@gmail.com>
2019-02-27 16:13:40 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -16,4 +16,10 @@ class Patient
     Appointment.new(date, self, doctor)
   end
   
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end 
+  
+  
+  
 end 
